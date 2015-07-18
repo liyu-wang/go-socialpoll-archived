a1e63444c2fc01613de21879319bd44bf7a1faa9
liyu-wang <liyu.wang@oasis.com>
2015-07-18 22:39:01 +1000

    connection

---
+++ twittervotes/twitter.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/joeshaw/envdecode"
+)
+
+var conn net.Conn
+var reader io.ReadCloser
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *httpClient
+)
+
+func dial(netw, addr string) (net.Conn, error) {
+	if conn != nil {
+		conn.Close()
+		conn = nil
+	}
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	conn = netc
+	return netc, nil
+}
+
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+}
+
+func setupTwitterAuth() {
+	// we don't need to use the type elsewhere,
+	// we define it inline as a anonymous type
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AcessSecret    string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AcessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnv := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnv)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
+}
