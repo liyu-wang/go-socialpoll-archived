c7d50173776c3ccbbeaaa89c9b80cf355dcee27c
liyu-wang <liyu.wang@oasis.com>
2015-07-23 22:13:07 +1000

    connecting to the db & consuming messages in NSQ

---
+++ counter/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/bitly/go-nsq"
+
+	"gopkg.in/mgo.v2"
+)
+
+var fatalErr error
+
+func fatal(e error) {
+	fmt.Println(e)
+	flag.PrintDefaults()
+	fatalErr = e
+}
+
+func main() {
+	defer func() {
+		if fatalErr != nil {
+			os.Exit(1)
+		}
+	}()
+
+	log.Println("Connecting to database...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	defer func() {
+		log.Println("Closing database connection...")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("polls")
+
+	var counts map[string]int
+	var countsLock sync.Mutex
+
+	log.Println("Connecting to nsq...")
+	// consumer for topic: votes channel: counter
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	// connecting to the http port of the nsqlookupd instance, rather than NSQ instances
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
+}
